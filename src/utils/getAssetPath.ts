40ed64bf5d603600e7aca0e8ebc5187b3242d95f
lemon <lemon@example.com>
2025-07-21 00:47:54 +0300

    add GitHub Actions deploy

--- src/utils/getAssetPath.ts
+++
@@ -1,8 +0,0 @@
-// export const getAssetPath = (relativePath: string): string => {
-//   // Для GitHub Pages используем явный базовый путь
-//   if (process.env.NODE_ENV === 'production') {
-//       return `/ai-chat-pages/${relativePath}`;
-//   }
-//   // Для разработки используем корневой путь
-//   return `/${relativePath}`;
-// };
