ec0b1e42b2d044e443e6edf60bbf4b23d8c93a42
lemon <lemon@example.com>
2025-09-01 12:29:26 +0300

    final no codes

--- src/app/epilogue/page.tsx
+++
@@ -1,7 +0,0 @@
-// ai-chat-page/src/app/page.tsx
-
-import ClientChatPage from "@features/common/ClientChatPage";
-
-export default function HomePage() {
-  return <ClientChatPage />;
-}
--- src/utils/formatFileUrl.ts
+++
@@ -1,3 +0,0 @@
-// ai-chat-page/src/utils/formatFileUrl.ts
-export const formatFileUrl = (fileUrl: string): string => 
-  `/${process.env.NODE_ENV === 'production' ? 'ai-chat-pages/' : ''}${fileUrl.replace(/^\//, '')}`;
